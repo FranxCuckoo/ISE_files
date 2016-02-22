97bdae902202403927d55e6e2678cc508e6f37c3
FranxCuckoo <tziambazis71@gmail.com>
2016-02-22 19:34:50 +0200

    Removed redundant signals and added another arch

--- PPDU.vhd
+++ PPDU.vhd
@@ -29,7 +29,6 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity PPDU is
     Port (  clk_250khz	: in STD_LOGIC; -- means to be 250khz
-			clk_200Mhz	: in STD_LOGIC;
 			reset		: in STD_LOGIC; -- active when 1
 			TX_enable	: in STD_LOGIC;	-- high enable shifting/outputting a bit at a time
 			bit_ppdu 	: out  STD_LOGIC); -- serial out
@@ -48,9 +47,6 @@ architecture BEHAV of PPDU is
 	-- Register the output
 	signal i_bit_ppdu : std_logic;
 
-	-- Frame control & seq_num bits
-	signal MPDU : std_logic_vector(39 downto 0); -- := (others => 'U');
-	
 	-- PPDU
 	signal PPDU : std_logic_vector(m-1 downto 0); -- := (others => 'U');
 	
@@ -85,3 +81,49 @@ architecture BEHAV of PPDU is
 			end if;
 		end process;
 	end BEHAV;
+
+
+--architecture BEHAV2 of PPDU is
+--	-- saves ppdu, shifting out one bit
+--	signal temp_register : std_logic_vector(m-1 downto 0); -- := (others => 'U');
+--	
+--	-- internal signal that has the value of temp_register(0)
+--	signal i_mux_in : std_logic;
+--
+--	-- internal signal that has the value of mux output
+--	signal i_mux_out : std_logic;
+--	
+--	-- Register the output
+--	signal i_bit_ppdu : std_logic;
+--
+--	-- Frame control & seq_num bits
+--	signal MPDU : std_logic_vector(39 downto 0); -- := (others => 'U');
+--	
+--	-- PPDU
+--	signal PPDU : std_logic_vector(m-1 downto 0); -- := (others => 'U');
+--	
+--	begin
+--		-- Connect i_bit_ppdu with bit_ppdu
+--		bit_ppdu <= i_bit_ppdu;
+--
+--		-- Creating MAC frame and PPDU frame
+--		PPDU <= PPDU_func(MPDU_frame);
+--
+--		main: process(clk_250khz)
+--		begin
+--			if rising_edge(clk_250khz) then
+--				if reset = '1' then
+--					i_bit_ppdu <= '0';
+--					temp_register <= PPDU;
+--				elsif TX_enable = '1' then
+--					i_bit_ppdu <= i_mux_in;
+--					
+--					i_mux_in <= temp_register(m-1); -- load new bit
+--					-- If i dont put &'0' it fills with 1 so it does numerical shift not logical that i want
+--					temp_register(m-1 downto 0) <= temp_register(m-2 downto 0) & '0';
+--				else -- TX_enable = '0' then
+--					i_bit_ppdu <= '0';
+--				end if;
+--			end if;
+--		end process;
+--	end BEHAV2;
